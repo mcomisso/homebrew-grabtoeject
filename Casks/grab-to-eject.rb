876e0131643f0e2b97d3b81537a738a5f46358cf
Matteo Comisso <teomatteo89@gmail.com>
2026-04-16 14:21:15 +0100

    Apply brew style fixes (stanza ordering, array alphabetization)

--- Casks/grab-to-eject.rb
+++ Casks/grab-to-eject.rb
@@ -8,11 +8,16 @@ cask "grab-to-eject" do
   desc "Safely ejects external drives when it detects you picking up your laptop"
   homepage "https://github.com/mcomisso/ShakeToEject"
 
+  # livecheck scans GitHub Releases for the newest vX.Y.Z tag.
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
   # The app uses Sparkle for in-app updates, so Homebrew does not need
   # to manage upgrades. This stanza also silences the "the app has a
   # mechanism for checking for updates" audit warning.
   auto_updates true
-
   depends_on macos: ">= :sonoma"
   depends_on arch: :arm64
 
@@ -21,16 +26,10 @@ cask "grab-to-eject" do
   app "ShakeToEject.app"
 
   zap trash: [
-    "~/Library/Preferences/com.mcsoftware.ShakeToEject.plist",
-    "~/Library/Saved Application State/com.mcsoftware.ShakeToEject.savedState",
+    "~/Library/Application Support/com.mcsoftware.ShakeToEject",
     "~/Library/Caches/com.mcsoftware.ShakeToEject",
     "~/Library/Caches/Sparkle_com.mcsoftware.ShakeToEject",
-    "~/Library/Application Support/com.mcsoftware.ShakeToEject",
+    "~/Library/Preferences/com.mcsoftware.ShakeToEject.plist",
+    "~/Library/Saved Application State/com.mcsoftware.ShakeToEject.savedState",
   ]
-
-  # livecheck scans GitHub Releases for the newest vX.Y.Z tag.
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
 end
